a0d25fbe4dd51c826d727081e64a6ab8ada26f18
Salma0801 <152594718+Salma0801@users.noreply.github.com>
2024-02-09 04:27:15 +0000

    basic js code config done

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,11 @@
+const accountId= 1234
+let accountEmail="sssalma@gmail.com"
+var accountPass="dsfjhg"
+// prefer not to use var becouse of issue in block scope and functional scope
+accountCity="Narsingdi"
+//accountId="11" // it not allowed
+accountEmail="salma@gmail.com"
+accountPass="1234"
+accountCity="Shibpur"
+console.log(accountId)
+console.table([accountId, accountEmail, accountPass, accountCity])
---
+++ 01_Basic/02_dataType.js
@@ -0,0 +1,14 @@
+"use strict" //treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs, not a browser
+
+console.log(3+
+    3) //code readability should be high
+
+// datatype
+//number, bigint, string, boolean, symbol=> unique, null=> type of object/ standalone value, undefined=> type of undefined
+
+//non-premitive data type
+//object
+console.log(typeof null)// object
+console.log(typeof undefined)//undefined
